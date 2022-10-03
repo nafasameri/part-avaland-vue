9ae155b8d2770b26357420ae1766e1a4570d192f
nafiseh.ameri <nafiseh.ameri@partsoftware.com>
2022-10-03 20:37:50 +0330

    feat: add music services and change user servies

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,5 @@
-import axios from "axios"
+import axios from "axios";
+import { useCookies } from 'vue3-cookies';
 
 const value = {
     namespaced: true,
@@ -15,20 +16,38 @@ const value = {
         async login(context, info) {
             let users = 'error';
             const data = JSON.stringify({
-                username: info.username ?? '',
-                password: info.password ?? ''
+                username: info.username ?? 'test',
+                password: info.password ?? '1111'
             });
-            console.log('info', info);
-            await axios.post('http://127.0.0.1:81/user/login', data, {
-                    'Content-Type': 'application/json'
-                })
+            console.log('data', data);
+            await axios.post('http://127.0.0.1:81/user/login', data)
                 .then(function (response) {
                     users = response.data.message;
+                    const { cookies } = useCookies();
+                    cookies.set('token', response.headers["set-cookie"]);
+        
                 })
                 .catch(function (error) {
                     users = error.response.data;
                 });
-            console.log(users);
+            console.log('users', users);
+            context.commit('LOGIN', users);
+        },
+        async signup(context, info) {
+            let users = 'error';
+            const data = JSON.stringify({
+                username: info.username ?? '',
+                password: info.password ?? ''
+            });
+            console.log('info', info);
+            await axios.post('http://127.0.0.1:81/user/signup', data)
+                .then(function (response) {
+                    users = response.data.message;
+                })
+                .catch(function (error) {
+                    users = error.response.data.message;
+                });
+            console.log('users', users);
             context.commit('LOGIN', users);
         }
     },
